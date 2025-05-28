df5721ded80d500af718b58cdd9f8d655fb432da
Luiz Otávio <luizotaviosilva492@gmail.com>
2025-05-28 20:50:36 -0300

    feat: adding entities

---
+++ SafeRoute.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace SafeRoute.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime RegisterDate { get; set; }
+}
