fc2055b1ab8cd8efcb26d5db759338decbcde8ac
Luiz Otávio <luizotaviosilva492@gmail.com>
2025-05-28 21:20:54 -0300

    feat: adding db connection

---
+++ SafeRoute.Infraestructure/Migrations/20250529001142_initDb.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafeRoute.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Type = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR2(500)", maxLength: 500, nullable: false),
+                    EventTime = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Latitude = table.Column<decimal>(type: "decimal(9,6)", nullable: false),
+                    Longitude = table.Column<decimal>(type: "decimal(9,6)", nullable: false),
+                    RiskLevel = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SafeResources",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR2(500)", maxLength: 500, nullable: false),
+                    Latitude = table.Column<decimal>(type: "decimal(9,6)", nullable: false),
+                    Longitude = table.Column<decimal>(type: "decimal(9,6)", nullable: false),
+                    Capacity = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SafeResources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Phone = table.Column<string>(type: "NVARCHAR2(20)", maxLength: 20, nullable: false),
+                    RegisterDate = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "SafeResources");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ SafeRoute.Ioc/Bootstrap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SafeRoute.Infraestructure.Data.AppData;
+
+namespace SafeRoute.Ioc;
+
+public class Bootstrap
+{
+    public static void Start(IServiceCollection service, IConfiguration configuration)
+    {
+        service.AddDbContext<ApplicationContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("Oracle");
+            options.UseOracle(connectionString);
+        });
+    }
+}
